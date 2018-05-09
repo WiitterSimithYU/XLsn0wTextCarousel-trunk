6c4097dab450cef8dfc2eba3d610d971fb7ae10c
WiitterSimithYU <378961614@qq.com>
2018-05-09 17:53:48 +0800

    comit

---
+++ README.md
@@ -0,0 +1,23 @@
+
+![image](https://github.com/XLsn0w/XLsn0wTextCarousel/blob/605aa82db23aad974e3c0e99ecb821ff4ee54759/XLsn0wTextCarousel.gif?raw=true)
+
+
+XLsn0wKit Objective-C Version
+=============
+![XLsn0wKit_objc](https://raw.githubusercontent.com/XLsn0w/XLsn0w/XLsn0w/XLsn0wLibrary/Resources/XLsn0wKit.png)
+![](https://github.com/XLsn0w/XLsn0wKit_objc/blob/master/pod.png?raw=true)
+
+AppleiOSDeveloper 
+====
+(Objective-CSwift) 
+——-
+(LearningKotlin/Java/JavaScript) 
+(+QQ946434404)
+
+# XLsn0w is An Apple Developer 
+# 简书:XLsn0w 
+# 新浪微博:XLsn0w 
+# 微信公众号:Cydiapple
+
+![image](https://raw.githubusercontent.com/XLsn0w/XLsn0w/XLsn0w/XLsn0wLibrary/Cydiapple.png)
+
---
+++ XLsn0w/XLsn0wTextCarousel/DataSourceModel.h
@@ -0,0 +1,24 @@
+/*********************************************************************************************
+ *   __      __   _         _________     _ _     _    _________   __         _         __   *
+ *	 \ \    / /  | |        | _______|   | | \   | |  |  ______ |  \ \       / \       / /   *
+ *	  \ \  / /   | |        | |          | |\ \  | |  | |     | |   \ \     / \ \     / /    *
+ *     \ \/ /    | |        | |______    | | \ \ | |  | |     | |    \ \   / / \ \   / /     *
+ *     /\/\/\    | |        |_______ |   | |  \ \| |  | |     | |     \ \ / /   \ \ / /      *
+ *    / /  \ \   | |______   ______| |   | |   \ \ |  | |_____| |      \ \ /     \ \ /       *
+ *   /_/    \_\  |________| |________|   |_|    \__|  |_________|       \_/       \_/        *
+ *                                                                                           *
+ *********************************************************************************************/
+
+#import <Foundation/Foundation.h>
+
+@interface DataSourceModel : NSObject
+
+@property (nonatomic, copy)   NSString *type;//类型 比如热门
+@property (nonatomic, strong) NSString *title;//滚动的文章标题
+@property (nonatomic, strong) NSString *URLString;//点击获取的URL
+
++ (instancetype)dataSourceModelWithType:(NSString *)type
+                                  title:(NSString *)title
+                              URLString:(NSString *)URLString;
+
+@end
